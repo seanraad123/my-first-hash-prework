818ce5b7b0c2c230597bc979ad9459847ec3aef6
Sean Conrad <seanconrad123@gmail.com>
2018-06-22 19:58:40 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {"name" => "Sean", "age" => 25, "wieght" => "145"}
 
-
 end
 
 
